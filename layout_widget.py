8852cd5007467ab621bf3660c7d224ad381adac3
Kevin te Raa <kevinteraa@gmail.com>
2021-07-24 17:10:29 +0200

    Split up the canvas content resizing logic into a seperate layout_widget and refactored the textbox to use its methods instead

---
+++ layout_widget.py
@@ -0,0 +1,92 @@
+from talon import Context, Module, actions, app, skia, cron, ctrl, scope, canvas, registry, settings, ui
+from talon.types import Point2d
+from abc import ABCMeta
+import numpy
+from user.talon_hud.base_widget import BaseWidget
+from user.talon_hud.utils import layout_rich_text, HudRichTextLine
+
+class LayoutWidget(BaseWidget):
+    """This widget has a layout pass and changes the canvas size based on the content"""
+    
+    resize_canvas_stage = 2
+    resize_stage_resize = 1
+    resize_stage_listener = 0
+    
+    def refresh(self, new_content):
+        self.resize_canvas_stage = 2
+    
+    def setup_move(self, pos):
+        # First resize the canvas back to the limit to make sure the content doesn't get clipped
+        #rect = ui.Rect(self.limit_x, self.limit_y, self.limit_width, self.limit_height)
+        #self.canvas.freeze()
+        #self.canvas.set_rect(rect)
+        
+        super().setup_move(pos)
+        
+    def layout_content(self, canvas, paint):
+        # Determine the dimensions and positions of the content
+        return {"rect": ui.Rect(self.limit_x, self.limit_y, self.limit_width, self.limit_height)}
+        
+    def draw_content(self, canvas, paint, dimensions) -> bool:
+        # Draw the content using the layout given dimensions
+        return False
+        
+    def draw(self, canvas) -> bool:
+        self.resize_canvas_stage = max(0, self.resize_canvas_stage - 1)
+        
+        # During any content or manual resizing, disable the mouse blocking behavior
+        if self.setup_type != "" and self.resize_canvas_stage == self.resize_stage_resize and self.mouse_enabled == True:
+            self.canvas.blocks_mouse = False
+            self.canvas.unregister('mouse', self.on_mouse)
+        
+        paint = self.draw_setup_mode(canvas)
+        
+        content_dimensions = self.layout_content(canvas, paint)
+        
+        # Debug layout size / position
+        #paint.color = "00FF00"
+        #paint.style = paint.Style.STROKE
+        #canvas.draw_rect(content_dimensions["rect"])        
+        
+        continue_drawing = self.draw_content(canvas, paint, content_dimensions)
+        
+        # Automatically resize the canvas based on the content to make sure no dead zones 
+        # will show up when clicking in or near the text panel
+        if self.setup_type == "":
+            if self.resize_canvas_stage == self.resize_stage_resize:
+                rect = content_dimensions["rect"]
+                self.canvas.set_rect(rect)
+            elif self.mouse_enabled == True and self.resize_canvas_stage == self.resize_stage_listener:
+                self.canvas.blocks_mouse = True
+                self.canvas.register('mouse', self.on_mouse)            
+
+        return self.resize_canvas_stage > 0 or continue_drawing
+        
+    def draw_rich_text(self, canvas, paint, rich_text, x, y, line_height, single_line=False):
+        # Draw text line by line
+        text_colour = self.theme.get_colour('text_box_colour')
+        error_colour = self.theme.get_colour('error_colour', 'AA0000')
+        warning_colour = self.theme.get_colour('warning_colour', 'F75B00')
+        success_colour = self.theme.get_colour('success_colour', '00CC00')
+        info_colour = self.theme.get_colour('info_colour', '30AD9E')    
+    
+        current_line = -1
+        for index, text in enumerate(rich_text):
+            paint.font.embolden = "bold" in text.styles
+            paint.font.skew_x = -0.33 if "italic" in text.styles else 0
+            paint.color = text_colour
+            if "warning" in text.styles:
+                paint.color = warning_colour
+            elif "success" in text.styles:
+                paint.color = success_colour
+            elif "error" in text.styles:
+                paint.color = error_colour
+            elif "info" in text.styles:
+                paint.color = info_colour
+            
+            current_line = current_line + 1 if text.x == 0 else current_line
+            if single_line and current_line > 0:
+                return
+            
+            text_y = y + line_height + current_line * line_height
+            canvas.draw_text(text.text, x + text.x, text_y )
--- widgets/textbox.py
+++ widgets/textbox.py
@@ -1,19 +1,21 @@
 from talon import skia, ui, Module, cron, actions
-from user.talon_hud.base_widget import BaseWidget
+from user.talon_hud.layout_widget import LayoutWidget
 from user.talon_hud.widget_preferences import HeadUpDisplayUserWidgetPreferences
 from user.talon_hud.utils import layout_rich_text, HudRichTextLine
 import numpy
 
-class HeadUpTextBox(BaseWidget):
+class HeadUpTextBox(LayoutWidget):
     preferences = HeadUpDisplayUserWidgetPreferences(type="text_box", x=1480, y=100, width=350, height=400, limit_x=1480, limit_y=100, limit_width=350, limit_height=400, enabled=False, alignment="left", expand_direction="down", font_size=18)
 
-    vertical_text_padding = 4
-    text_padding = 8
-    text_padding_right = 20
+    mouse_enabled = True
+
+    # Top, right, bottom, left, same order as CSS padding
+    padding = [3, 20, 10, 8]     
+    line_padding = 8
     
     close_button_radius = 10
     close_icon_position = [0,0]
-    close_icon_hovered = False    
+    close_icon_hovered = False
 
     subscribed_content = ["mode", "text_state"]
     content = {
@@ -22,14 +24,6 @@ class HeadUpTextBox(BaseWidget):
         'text_state': " "
     }
     animation_max_duration = 60
-    
-    # Content resize flags
-    resize_canvas_stage = 2
-    resize_stage_resize = 1 # Remove the listeners and resize the canvas
-    resize_stage_listener = 0 # Append the mouse click listener again here
-    
-    def refresh(self, new_content):
-        self.resize_canvas_stage = 2
     
     def on_mouse(self, event):
         pos = numpy.array(event.gpos)
@@ -46,35 +40,62 @@ class HeadUpTextBox(BaseWidget):
                 self.close_icon_hovered = False
                 self.canvas.resume()
     
-    def draw(self, canvas) -> bool:
-        self.resize_canvas_stage = max(0, self.resize_canvas_stage - 1)
-        
-        # During any content or manual resizing, disable the mouse blocking behavior
-        if self.setup_type != "" and self.resize_canvas_stage == self.resize_stage_resize:
-            self.canvas.blocks_mouse = False
-            self.canvas.unregister('mouse', self.on_mouse)
+    def layout_content(self, canvas, paint):
+        paint.textsize = self.font_size    
     
-        paint = self.draw_setup_mode(canvas)
-        header_height = 30
-        paint.textsize = self.font_size
-
-        # Do a layout pass to determine the size of the content
-        dimensions, header_text, content_text = self.layout_content(canvas, paint)
-        background_height = self.draw_content(header_height, canvas, paint, dimensions, content_text)
-        self.draw_header(header_height, canvas, paint, dimensions, header_text)
+        """Calculates the width and the height of the content"""
+        header_text = layout_rich_text(paint, self.content['textbox_header'], self.limit_width - self.close_button_radius * 1.5, self.limit_height)    
+        content_text = layout_rich_text(paint, self.content['text_state'], self.limit_width, self.limit_height)
         
-        # Automatically resize the canvas based on the content to make sure no dead zones 
-        # will show up when clicking in or near the text panel
-        if self.setup_type == "":
-            if self.resize_canvas_stage == self.resize_stage_resize:
-                x = self.x if self.alignment == "left" else self.limit_x + self.limit_width - dimensions.width - self.text_padding_right - self.text_padding
-                rect = ui.Rect(x, self.y, dimensions.width + self.text_padding_right + self.text_padding, background_height)
-                self.canvas.set_rect(rect)
-            elif self.resize_canvas_stage == self.resize_stage_listener:
-                self.canvas.blocks_mouse = True
-                self.canvas.register('mouse', self.on_mouse)            
-
-        return self.resize_canvas_stage > 0
+        line_count = 0
+        total_text_width = 0
+        total_text_height = 0
+        current_line_length = 0
+        header_height = 0
+        
+        # The header can only be one line long, so only take the first line
+        for index, text in enumerate(header_text):
+            line_count = line_count + 1 if text.x == 0 else line_count
+            if line_count <= 1:
+                header_height = text.height + self.padding[0] * 2
+                current_line_length = current_line_length + text.width + self.close_button_radius * 1.5
+                total_text_width = max( total_text_width, current_line_length )
+        header_height = max(header_height, self.padding[0] * 2 + self.close_button_radius)
+        
+        line_count = 0        
+        for index, text in enumerate(content_text):
+            line_count = line_count + 1 if text.x == 0 else line_count
+            current_line_length = current_line_length + text.width if text.x != 0 else text.width
+            total_text_width = max( total_text_width, current_line_length )
+            total_text_height = total_text_height + text.height + self.line_padding if text.x == 0 else total_text_height        
+        
+        width = max(self.width, total_text_width + self.padding[1] + self.padding[3])
+        height = max(self.height, total_text_height + self.padding[0] + self.padding[2] + header_height)
+        x = self.x if self.alignment == "left" else self.limit_x + self.limit_width - width
+        y = self.y if self.expand_direction == "down" else self.limit_y + self.limit_height - height
+        
+        return {
+            "rect": ui.Rect(x, y, width, height), 
+            "line_count": max(1, line_count),
+            "header_text": header_text,
+            "content_text": content_text,
+            "header_height": header_height
+        }
+    
+    def draw_content(self, canvas, paint, dimensions) -> bool:
+        paint.textsize = self.font_size
+        
+        paint.style = paint.Style.FILL
+        
+        # Draw the background first
+        background_colour = self.theme.get_colour('text_box_background', 'F5F5F5')
+        paint.color = background_colour
+        self.draw_background(canvas, paint, dimensions["rect"])
+        
+        background_height = self.draw_content_text(canvas, paint, dimensions)
+        self.draw_header(canvas, paint, dimensions)
+        
+        return False
 
     def draw_animation(self, canvas, animation_tick):
         if self.enabled:
@@ -82,116 +103,45 @@ class HeadUpTextBox(BaseWidget):
         else:
             return self.draw(canvas)
     
-    def layout_content(self, canvas, paint) -> (ui.Rect, list[HudRichTextLine], list[HudRichTextLine]):
-        """Calculates the width and the height of the content"""
-        header_text = layout_rich_text(paint, self.content['textbox_header'], self.limit_width - self.close_button_radius * 1.5, self.limit_height)
-        content_text = layout_rich_text(paint, self.content['text_state'], self.limit_width - self.text_padding_right * 2, self.limit_height)
-
-        total_text_width = 0
-        total_text_height = 0
-        current_line_length = self.close_button_radius * 4
-        line_count = 0
+    def draw_header(self, canvas, paint, dimensions):
+        header_height = dimensions["header_height"]
+        dimensions = dimensions["rect"]
         
-        # The header can only be one line long, so only take the first line
-        for index, text in enumerate(header_text):
-            line_count = line_count + 1 if text.x == 0 else line_count
-            if line_count <= 1:
-                current_line_length = current_line_length + text.width
-                total_text_width = max( total_text_width, current_line_length )
-        
-        line_count = 0
-        for index, text in enumerate(content_text):
-            line_count = line_count + 1 if text.x == 0 else line_count
-            current_line_length = current_line_length + text.width if text.x != 0 else text.width
-            total_text_width = max( total_text_width, current_line_length )
-            total_text_height = total_text_height + text.height + self.vertical_text_padding if text.x == 0 else total_text_height
-
-        return ui.Rect(0, max(1, line_count), total_text_width, total_text_height), header_text, content_text
-
-    def draw_header(self, header_height, canvas, paint, dimensions, rich_text):
         paint.color = self.theme.get_colour('text_colour')
         paint.font.embolden = True
         
         # TODO RICH TEXT HEADER
-        x = self.x + self.text_padding if self.alignment == "left" else self.limit_x + self.limit_width - dimensions.width - self.text_padding
-        canvas.draw_text(self.content['textbox_header'], x, self.y + self.font_size)
+        x = dimensions.x + self.padding[3]
+        canvas.draw_text(self.content['textbox_header'], x, dimensions.y + self.font_size)
         
         # Small divider between the content and the header
-        canvas.draw_rect(ui.Rect(x - self.text_padding, self.y + self.font_size + self.text_padding, 
-            dimensions.width + self.text_padding_right if self.alignment == "left" 
-            else dimensions.width + self.text_padding_right - self.text_padding, 1))
+        canvas.draw_rect(ui.Rect(x - self.padding[3], dimensions.y + header_height + self.padding[0] * 2, dimensions.width, 1))
         close_colour = self.theme.get_colour('close_icon_hover_colour') if self.close_icon_hovered else self.theme.get_colour('close_icon_accent_colour')
         paint.style = paint.Style.FILL
         paint.shader = skia.Shader.linear_gradient(self.x, self.y, self.x, self.y + header_height, (self.theme.get_colour('close_icon_colour'), close_colour), None)
         
         # Closing button
-        x = self.x + self.text_padding if self.alignment == "left" else self.limit_x + self.limit_width - dimensions.width - self.close_button_radius * 1.5 - self.text_padding
-        self.close_icon_position = [x + dimensions.width + self.text_padding_right - self.close_button_radius * 1.5,
-            self.y + self.close_button_radius + self.vertical_text_padding / 2]
+        x = dimensions.x
+        self.close_icon_position = [x + dimensions.width - self.close_button_radius * 1.5,
+            dimensions.y + self.close_button_radius + self.padding[0] / 2]
         canvas.draw_circle(self.close_icon_position[0], self.close_icon_position[1], self.close_button_radius, paint)
 
-    def draw_content(self, header_height, canvas, paint, dimensions, rich_text) -> int:
+    def draw_content_text(self, canvas, paint, dimensions) -> int:
         """Draws the content and returns the height of the drawn content"""
         paint.textsize = self.font_size
-
-        background_colour = self.theme.get_colour('text_box_background', 'F5F5F5')
-        paint.color = background_colour
-        text_padding = self.text_padding
         
-        current_y = self.y + header_height if self.expand_direction == "down" else self.y + self.height                        
-        log_height = self.vertical_text_padding * (1 + dimensions.y) + dimensions.height
-    
-        if self.expand_direction == "down":
-            offset = 0
-            current_y = current_y + offset
-        else:
-            offset = log_height
-            current_y = current_y - offset
-        
-        text_width = dimensions.width
-        element_width = dimensions.width if self.alignment == "left" else dimensions.width - self.text_padding
+        header_height = dimensions["header_height"]
+        rich_text = dimensions["content_text"]
+        line_count = dimensions["line_count"]
+        dimensions = dimensions["rect"]
+       
+        text_x = dimensions.x + self.padding[3]
+        text_y = dimensions.y + header_height + self.padding[0] * 2
+       
+        line_height = ( dimensions.height - header_height - self.padding[0] - self.padding[2] ) / line_count
+        self.draw_rich_text(canvas, paint, rich_text, text_x, text_y, line_height)
 
-        text_x = self.x + text_padding if self.alignment == "left" else self.limit_x + self.limit_width - text_padding - text_width
-        element_x = text_x - text_padding
-        
-        self.draw_background(canvas, element_x, current_y - header_height, element_width + self.text_padding_right, log_height + header_height, paint)
-        line_height = dimensions.height / dimensions.y
-        self.draw_rich_text(canvas, paint, rich_text, text_x, current_y, line_height, self.vertical_text_padding)
-        return log_height + header_height
-
-
-    def draw_rich_text(self, canvas, paint, rich_text, x, y, line_height, padding, single_line=False):
-        # Draw text line by line
-        text_colour = self.theme.get_colour('text_box_colour')
-        error_colour = self.theme.get_colour('error_colour', 'AA0000')
-        warning_colour = self.theme.get_colour('warning_colour', 'F75B00')
-        success_colour = self.theme.get_colour('success_colour', '00CC00')
-        info_colour = self.theme.get_colour('info_colour', '30AD9E')    
-    
-        current_line = -1
-        for index, text in enumerate(rich_text):
-            paint.font.embolden = "bold" in text.styles
-            paint.font.skew_x = -0.33 if "italic" in text.styles else 0
-            paint.color = text_colour
-            if "warning" in text.styles:
-                paint.color = warning_colour
-            elif "success" in text.styles:
-                paint.color = success_colour
-            elif "error" in text.styles:
-                paint.color = error_colour
-            elif "info" in text.styles:
-                paint.color = info_colour
-            
-            current_line = current_line + 1 if text.x == 0 else current_line
-            if single_line and current_line > 0:
-                return
-            
-            text_y = y + line_height + current_line * padding + current_line * line_height
-            canvas.draw_text(text.text, x + text.x, text_y )
-
-
-    def draw_background(self, canvas, origin_x, origin_y, width, height, paint):
+    def draw_background(self, canvas, paint, rect):
         radius = 10
-        rect = ui.Rect(origin_x, origin_y, width, height)
         rrect = skia.RoundRect.from_rect(rect, x=radius, y=radius)
         canvas.draw_rrect(rrect)
