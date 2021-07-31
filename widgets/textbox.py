8bf6a1c8a51d8cb425c0e14f3bd9f19c063f0cc6
Kevin te Raa <kevinteraa@gmail.com>
2021-07-31 11:55:33 +0200

    Improved the textbox icon rendering and added minimization and maximization options

--- widgets/textbox.py
+++ widgets/textbox.py
@@ -6,16 +6,23 @@ import numpy
 
 class HeadUpTextBox(LayoutWidget):
     preferences = HeadUpDisplayUserWidgetPreferences(type="text_box", x=1630, y=100, width=200, height=200, limit_x=1530, limit_y=100, limit_width=350, limit_height=400, enabled=False, alignment="left", expand_direction="down", font_size=18)
-
+    minimized = False
     mouse_enabled = True
 
     # Top, right, bottom, left, same order as CSS padding
     padding = [3, 20, 10, 8]     
     line_padding = 8
     
-    close_button_radius = 10
-    close_icon_position = [0,0]
-    close_icon_hovered = False
+    # All the header icons in a right to left order
+    icon_radius = 10
+    icon_hovered = -1
+    icons = [{
+        "type": "close",
+        "pos": [0,0]        
+    },{
+        "type": "minimize",
+        "pos": [0,0]
+    }]
 
     subscribed_content = ["mode", "text_state"]
     content = {
@@ -28,18 +35,23 @@ class HeadUpTextBox(LayoutWidget):
     def on_mouse(self, event):
         pos = numpy.array(event.gpos)
         
-        if (numpy.linalg.norm(pos - numpy.array([self.close_icon_position[0], self.close_icon_position[1]])) 
-            < self.close_button_radius):
-            if not self.close_icon_hovered:
-                self.close_icon_hovered = True
+        icon_hovered = -1
+        for index, icon in enumerate(self.icons):
+            if (numpy.linalg.norm(pos - numpy.array([icon['pos'][0], icon['pos'][1]])) < self.icon_radius):
+                icon_hovered = index
+
+        if icon_hovered != self.icon_hovered:
+            self.icon_hovered = icon_hovered
+            self.canvas.resume()
+        
+        if event.event == "mouseup" and event.button == 0 and icon_hovered != -1:
+            if self.icons[icon_hovered]['type'] == "close":
+                self.disable()
+            elif self.icons[icon_hovered]['type'] == "minimize":
+                self.minimized = not self.minimized
+                self.mark_layout_invalid = True
                 self.canvas.resume()
-            if (event.event == "mouseup" and event.button == 0):
-                self.disable(True)
-        else: 
-            if self.close_icon_hovered:
-                self.close_icon_hovered = False
-                self.canvas.resume()
-    
+                
     def layout_content(self, canvas, paint):
         paint.textsize = self.font_size
         
@@ -48,10 +60,11 @@ class HeadUpTextBox(LayoutWidget):
     
         layout_width = max(self.width - self.padding[1] - self.padding[3] * 2, 
             self.limit_width - self.padding[1] * 2 - self.padding[3] * 2)
+        icon_size = len(self.icons) * 2 * self.icon_radius
     
         """Calculates the width and the height of the content"""
-        header_text = layout_rich_text(paint, self.content['textbox_header'], self.limit_width - self.close_button_radius * 1.5, self.limit_height)    
-        content_text = layout_rich_text(paint, self.content['text_state'], layout_width, self.limit_height)
+        header_text = layout_rich_text(paint, self.content['textbox_header'], self.limit_width - icon_size, self.limit_height)
+        content_text = [] if self.minimized else layout_rich_text(paint, self.content['text_state'], layout_width, self.limit_height)
         
         line_count = 0
         total_text_width = 0
@@ -64,20 +77,22 @@ class HeadUpTextBox(LayoutWidget):
             line_count = line_count + 1 if text.x == 0 else line_count
             if line_count <= 1:
                 header_height = text.height + self.padding[0] * 2
-                current_line_length = current_line_length + text.width + self.close_button_radius * 1.5
+                current_line_length = current_line_length + text.width + self.icon_radius * 1.5
                 total_text_width = max( total_text_width, current_line_length )
-        header_height = max(header_height, self.padding[0] * 2 + self.close_button_radius)
+        header_height = max(header_height, self.padding[0] * 2 + self.icon_radius)
         
-        line_count = 0        
-        for index, text in enumerate(content_text):
-            line_count = line_count + 1 if text.x == 0 else line_count
-            current_line_length = current_line_length + text.width if text.x != 0 else text.width
-            total_text_width = max( total_text_width, current_line_length )
-            total_text_height = total_text_height + text.height + self.line_padding if text.x == 0 else total_text_height        
-        
+        # We do not render content if the text box is minimized
+        if not self.minimized:
+            line_count = 0        
+            for index, text in enumerate(content_text):
+                line_count = line_count + 1 if text.x == 0 else line_count
+                current_line_length = current_line_length + text.width if text.x != 0 else text.width
+                total_text_width = max( total_text_width, current_line_length )
+                total_text_height = total_text_height + text.height + self.line_padding if text.x == 0 else total_text_height        
+            
         width = min( self.limit_width, max(self.width, total_text_width + self.padding[1] + self.padding[3]))
-        content_height = total_text_height + self.padding[0] + self.padding[2] + header_height
-        height = min(self.limit_height, max(self.height, content_height))
+        content_height = header_height if self.minimized else total_text_height + self.padding[0] + self.padding[2] + header_height
+        height = header_height + self.padding[0] * 2 if self.minimized else min(self.limit_height, max(self.height, content_height))
         x = self.x if horizontal_alignment == "left" else self.limit_x + self.limit_width - width
         y = self.limit_y if vertical_alignment == "top" else self.limit_y + self.limit_height - height
         
@@ -85,6 +100,7 @@ class HeadUpTextBox(LayoutWidget):
             "rect": ui.Rect(x, y, width, height), 
             "line_count": max(1, line_count),
             "header_text": header_text,
+            "icon_size": icon_size,
             "content_text": content_text,
             "header_height": header_height,
             "content_height": content_height
@@ -118,21 +134,39 @@ class HeadUpTextBox(LayoutWidget):
         paint.color = self.theme.get_colour('text_colour')
         paint.font.embolden = True
         
-        # TODO RICH TEXT HEADER
+        # TODO RICH TEXT HEADER?
         x = dimensions.x + self.padding[3]
         canvas.draw_text(self.content['textbox_header'], x, dimensions.y + self.font_size)
         
         # Small divider between the content and the header
-        canvas.draw_rect(ui.Rect(x - self.padding[3], dimensions.y + header_height + self.padding[0] * 2, dimensions.width, 1))
-        close_colour = self.theme.get_colour('close_icon_hover_colour') if self.close_icon_hovered else self.theme.get_colour('close_icon_accent_colour')
-        paint.style = paint.Style.FILL
-        paint.shader = skia.Shader.linear_gradient(self.x, self.y, self.x, self.y + header_height, (self.theme.get_colour('close_icon_colour'), close_colour), None)
+        if not self.minimized:
+            canvas.draw_rect(ui.Rect(x - self.padding[3], dimensions.y + header_height + self.padding[0] * 2, dimensions.width, 1))
         
-        # Closing button
+        # Header button tray 
         x = dimensions.x
-        self.close_icon_position = [x + dimensions.width - self.close_button_radius * 1.5,
-            dimensions.y + self.close_button_radius + self.padding[0] / 2]
-        canvas.draw_circle(self.close_icon_position[0], self.close_icon_position[1], self.close_button_radius, paint)
+        for index, icon in enumerate(self.icons):
+            # Minimize / maximize icon
+            icon_position = [x + dimensions.width - (self.icon_radius * 1.5 + ( index * self.icon_radius * 2.2 )),
+                dimensions.y + self.icon_radius + self.padding[0] / 2]
+            self.icons[index]['pos'] = icon_position
+            paint.style = paint.Style.FILL
+            if icon['type'] == "minimize":
+            
+                hover_colour = '999999' if self.icon_hovered == index else 'CCCCCC'            
+                paint.shader = skia.Shader.linear_gradient(self.x, self.y, self.x, self.y + header_height, ('AAAAAA', hover_colour), None)
+                canvas.draw_circle(icon_position[0], icon_position[1], self.icon_radius, paint)
+                paint.shader = skia.Shader.linear_gradient(self.x, self.y, self.x, self.y + header_height, ('000000', '000000'), None)
+        
+                if not self.minimized:
+                    canvas.draw_rect(ui.Rect(icon_position[0] - self.icon_radius / 2, icon_position[1] - 1, self.icon_radius, 2))
+                else:
+                    paint.style = paint.Style.STROKE
+                    canvas.draw_rect(ui.Rect( 1 + icon_position[0] - self.icon_radius / 2, icon_position[1] - self.icon_radius / 2, 
+                        self.icon_radius - 2, self.icon_radius - 2))
+            elif icon['type'] == "close":
+                close_colour = self.theme.get_colour('close_icon_hover_colour') if self.icon_hovered == index else self.theme.get_colour('close_icon_accent_colour')            
+                paint.shader = skia.Shader.linear_gradient(self.x, self.y, self.x, self.y + header_height, (self.theme.get_colour('close_icon_colour'), close_colour), None)
+                canvas.draw_circle(icon_position[0], icon_position[1], self.icon_radius, paint)
 
     def draw_content_text(self, canvas, paint, dimensions) -> int:
         """Draws the content and returns the height of the drawn content"""
