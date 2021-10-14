34cc543363b97a36e27111038efa9036f9a09691
Kevin te Raa <kevinteraa@gmail.com>
2021-10-14 20:28:39 +0200

    Finalized basic dictation walkthrough

--- docs/load_talon_hud_docs.py
+++ docs/load_talon_hud_docs.py
@@ -3,7 +3,7 @@ import os
 
 def talon_hud_ready():
     # Check if Talon HUD is available to the user
-    if "user.talon_hud_available" in scope.get('tag'):
+    if "user.talon_hud_available" in scope.get("tag"):
     
         # Get the absolute path to the documentation directory for your package
         documentation_dir = str(os.path.dirname(os.path.abspath(__file__)))
@@ -14,12 +14,12 @@ def talon_hud_ready():
             documentation_dir + "/hud_widget_documentation.txt")
 
         # Add a walkthrough to the Toolkit walkthrough options
-        actions.user.hud_add_walkthrough('Head up display', 
-            documentation_dir + '/hud_walkthrough.json')
+        actions.user.hud_add_walkthrough("Head up display", 
+            documentation_dir + "/hud_walkthrough.json")
         
         # Dictation
-        actions.user.hud_add_walkthrough('Dictating sentences and words', 
-            documentation_dir + '/dictation_walkthrough.json')
+        actions.user.hud_add_walkthrough("Basic sentence dictation", 
+            documentation_dir + "/dictation_walkthrough.json")
         
         # Browser documentation
         actions.user.hud_add_documentation("Browser usage", 
@@ -27,12 +27,12 @@ def talon_hud_ready():
             documentation_dir + "/browser_usage_documentation.txt")
             
         # Basic browser usage
-        actions.user.hud_add_walkthrough('Basic browser usage', 
-            documentation_dir + '/basic_browser_usage_walkthrough.json')
+        actions.user.hud_add_walkthrough("Basic browser usage", 
+            documentation_dir + "/basic_browser_usage_walkthrough.json")
             
         # Media usage
-        actions.user.hud_add_walkthrough('Music and video controls', 
-            documentation_dir + '/music_and_video_walkthrough.json')
+        actions.user.hud_add_walkthrough("Music and video controls", 
+            documentation_dir + "/music_and_video_walkthrough.json")
             
 
-app.register('ready', talon_hud_ready)
+app.register("ready", talon_hud_ready)
